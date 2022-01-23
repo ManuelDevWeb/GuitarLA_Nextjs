de8234ed74d5adcb3319cc36245992354cd6fd17
Manuel V <manueldeveloper17@gmail.com>
2022-01-23 18:42:36 -0500

    Se creo el Layou y se finalizó el componente Header

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
+
+// Styles
+import styles from "../styles/Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className="contenedor">
+        <div className={styles.barra}>
+          {/* Logo */}
+          <div>
+            <Link href="/">
+              <Image
+                src="/img/logo.svg"
+                width={400}
+                height={100}
+                alt="Imagen Logo"
+              />
+            </Link>
+          </div>
+
+          {/* Navegación */}
+          <nav className={styles.navegacion}>
+            <Link href="/">Inicio</Link>
+            <Link href="/nosotros">Nosotros</Link>
+            <Link href="/blog">Blog</Link>
+            <Link href="/tienda">Tienda</Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
