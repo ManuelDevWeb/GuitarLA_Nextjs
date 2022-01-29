d6ccb99ce4896930ac5865dac6e381ba04939792
Manuel V <manueldeveloper17@gmail.com>
2022-01-29 01:50:28 -0500

    Finalizando sección Home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 // Styles
 import styles from "../styles/Header.module.css";
 
-const Header = () => {
+const Header = ({ guitarra }) => {
+  const router=useRouter();
+
   return (
     <header className={styles.header}>
       <div className="contenedor">
@@ -31,7 +34,24 @@ const Header = () => {
             <Link href="/tienda">Tienda</Link>
           </nav>
         </div>
+
+        {guitarra && (
+          <div className={styles.modelo}>
+            <h2>Modelo {guitarra.nombre}</h2>
+            <p>{guitarra.descripcion}</p>
+            <p className={styles.precio}>${guitarra.precio}</p>
+            <Link href={`/guitarra/${guitarra.url}`}>
+              <a className={styles.enlace}>Ver Producto</a>
+            </Link>
+          </div>
+        )}
       </div>
+
+      {router.pathname==='/' && (
+        <div className={styles.guitarra}>
+          <Image layout="fixed" width={500} height={1200} src="/img/header_guitarra.png" alt="Imagen Header Guitarra" />
+        </div>        
+      )}
     </header>
   );
 };
